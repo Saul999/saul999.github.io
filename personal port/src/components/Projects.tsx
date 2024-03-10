2b132d4dddb6732b9f9cf579000601a510831a34
Saul999 <84159357+Saul999@users.noreply.github.com>
2024-03-10 03:12:03 -0700

    Update Projects.tsx

--- personal port/src/components/Projects.tsx
+++ personal port/src/components/Projects.tsx
@@ -32,88 +32,105 @@ function Project() {
             className="w-1/4 rounded-lg bg-brown-400 ml-auto"
           />
         </div>
-        {/* Project Card 2 */}
-        <div className="bg-gray-100 p-4 rounded-lg mb-4">
-          <img
-            src="project2.jpg"
-            alt="Project 2"
-            className="w-full h-auto rounded-lg mb-4"
-          />
-          <div>
-            <h3 className="text-lg font-bold">Project 2</h3>
-            <p className="text-sm">Description of Project 2</p>
+        <div className="bg-blue-700 p-4 rounded-lg mb-4 flex">
+          <div className="flex flex-col w-3/4">
+            <h3 className="text-lg font-bold bg-yellow-800">Teach</h3>
+            <p className="text-sm bg-lime-400">
+              An interactive, community-driven, informative music website with
+              data for more than 800+ unreleased songs.
+            </p>
+            <div className="flex">
+              <div className="w-10 bg-zinc-700 flex justify-between items-center">
+                <FaGithubSquare />
+                <FaExternalLinkAlt />
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="bg-gray-100 p-4 rounded-lg mb-4">
-          <img
-            src="project1.jpg"
-            alt="Project 1"
-            className="w-full h-auto rounded-lg mb-4"
-          />
-          <div>
-            <h3 className="text-lg font-bold">Project 1</h3>
-            <p className="text-sm">Description of Project 1</p>
-          </div>
-        </div>
-        {/* Project Card 2 */}
-        <div className="bg-gray-100 p-4 rounded-lg mb-4">
-          <img
-            src="project2.jpg"
-            alt="Project 2"
-            className="w-full h-auto rounded-lg mb-4"
-          />
-          <div>
-            <h3 className="text-lg font-bold">Project 2</h3>
-            <p className="text-sm">Description of Project 2</p>
-          </div>
-        </div>
-        <div className="bg-gray-100 p-4 rounded-lg mb-4">
           <img
             src="project1.jpg"
-            alt="Project 1"
-            className="w-full h-auto rounded-lg mb-4"
+            alt="JuiceHub"
+            className="w-1/4 rounded-lg bg-brown-400 ml-auto"
           />
-          <div>
-            <h3 className="text-lg font-bold">Project 1</h3>
-            <p className="text-sm">Description of Project 1</p>
-          </div>
         </div>
-        {/* Project Card 2 */}
-        <div className="bg-gray-100 p-4 rounded-lg mb-4">
-          <img
-            src="project2.jpg"
-            alt="Project 2"
-            className="w-full h-auto rounded-lg mb-4"
-          />
-          <div>
-            <h3 className="text-lg font-bold">Project 2</h3>
-            <p className="text-sm">Description of Project 2</p>
+        <div className="bg-blue-700 p-4 rounded-lg mb-4 flex">
+          <div className="flex flex-col w-3/4">
+            <h3 className="text-lg font-bold bg-yellow-800">JuiceHUB</h3>
+            <p className="text-sm bg-lime-400">
+              An interactive, community-driven, informative music website with
+              data for more than 800+ unreleased songs.
+            </p>
+            <div className="flex">
+              <div className="w-10 bg-zinc-700 flex justify-between items-center">
+                <FaGithubSquare />
+                <FaExternalLinkAlt />
+              </div>
+            </div>
           </div>
+          <img
+            src="project1.jpg"
+            alt="JuiceHub"
+            className="w-1/4 rounded-lg bg-brown-400 ml-auto"
+          />
         </div>
-        {/* Add more project cards as needed */}
-        {/* Project Card 2 */}
-        <div className="bg-gray-100 p-4 rounded-lg mb-4">
-          <img
-            src="project2.jpg"
-            alt="Project 2"
-            className="w-full h-auto rounded-lg mb-4"
-          />
-          <div>
-            <h3 className="text-lg font-bold">Project 2</h3>
-            <p className="text-sm">Description of Project 2</p>
+        <div className="bg-blue-700 p-4 rounded-lg mb-4 flex">
+          <div className="flex flex-col w-3/4">
+            <h3 className="text-lg font-bold bg-yellow-800">JuiceHUB</h3>
+            <p className="text-sm bg-lime-400">
+              An interactive, community-driven, informative music website with
+              data for more than 800+ unreleased songs.
+            </p>
+            <div className="flex">
+              <div className="w-10 bg-zinc-700 flex justify-between items-center">
+                <FaGithubSquare />
+                <FaExternalLinkAlt />
+              </div>
+            </div>
           </div>
+          <img
+            src="project1.jpg"
+            alt="JuiceHub"
+            className="w-1/4 rounded-lg bg-brown-400 ml-auto"
+          />
         </div>
-        {/* Project Card 2 */}
-        <div className="bg-gray-100 p-4 rounded-lg mb-4">
-          <img
-            src="project2.jpg"
-            alt="Project 2"
-            className="w-full h-auto rounded-lg mb-4"
-          />
-          <div>
-            <h3 className="text-lg font-bold">Project 2</h3>
-            <p className="text-sm">Description of Project 2</p>
+        <div className="bg-blue-700 p-4 rounded-lg mb-4 flex">
+          <div className="flex flex-col w-3/4">
+            <h3 className="text-lg font-bold bg-yellow-800">JuiceHUB</h3>
+            <p className="text-sm bg-lime-400">
+              An interactive, community-driven, informative music website with
+              data for more than 800+ unreleased songs.
+            </p>
+            <div className="flex">
+              <div className="w-10 bg-zinc-700 flex justify-between items-center">
+                <FaGithubSquare />
+                <FaExternalLinkAlt />
+              </div>
+            </div>
           </div>
+          <img
+            src="images/juiceHubSS.png"
+            alt="JuiceHub"
+            className="w-1/4 rounded-lg bg-brown-400 ml-auto"
+          />
+        </div>
+        <div className="bg-blue-700 p-4 rounded-lg mb-4 flex">
+          <div className="flex flex-col w-3/4">
+            <h3 className="text-lg font-bold bg-yellow-800">JuiceHUB</h3>
+            <p className="text-sm bg-lime-400">
+              An interactive, community-driven, informative music website with
+              data for more than 800+ unreleased songs.
+            </p>
+            <div className="flex">
+              <div className="w-10 bg-zinc-700 flex justify-between items-center">
+                <FaGithubSquare />
+                <FaExternalLinkAlt />
+              </div>
+            </div>
+          </div>
+          <img
+            src="project1.jpg"
+            alt="JuiceHub"
+            className="w-1/4 rounded-lg bg-brown-400 ml-auto"
+          />
         </div>
       </div>
     </div>
