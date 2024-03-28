0838177c3fd9f8e836f309480df47aa663dfdf13
Saul385 <84159357+Saul999@users.noreply.github.com>
2024-03-27 19:16:10 -0700

    update about me desc

--- personal port/src/components/About.tsx
+++ personal port/src/components/About.tsx
@@ -9,20 +9,23 @@ function About() {
         <h1 className="text-3xl font-bold mb-2">Saul Ruiz</h1>
         <h2 className="text-2xl mb-1">Aspiring Software Engineer</h2>
         <p className="text-lg">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultricies
-          erat eget nisl volutpat vulputate. In hac habitasse platea dictumst.
-          Nulla facilisi.
+          Current Senior at CSUF pursuing a Computer Science Degree. Starting
+          coding in 2019 learning Java, then C++ in 2020, and stuck with Python
+          in 2021. Currently learning Typescript while building the JuiceHub and
+          improving my Python skills.
         </p>
-
-        <p className="py-9">-Projects</p>
       </div>
 
       <div className="mt-auto flex">
-        <a href="https://github.com/Saul999/JuiceHub" className="flex">
+        <a
+          href="https://github.com/Saul999/JuiceHub"
+          target={"_blank"}
+          className="flex"
+        >
           {" "}
           <FaGithubSquare size={24} color="white" />
         </a>
-        <a href="https://www.linkedin.com/in/saul385/">
+        <a href="https://www.linkedin.com/in/saul385/" target={"_blank"}>
           <FaLinkedin size={24} color="white" />
         </a>
       </div>
